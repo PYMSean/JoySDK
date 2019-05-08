87d97a84b23bb347b1c0e763c46c6e3e72a2d6a0
joy <joy@joydeMacBook-Pro.local>
2019-05-08 17:56:33 +0800

    add new file

---
+++ JoySDK/Aggregate/Person.m
@@ -0,0 +1,18 @@
+//
+//  Person.m
+//  Aggregate
+//
+//  Created by joy on 2019/3/27.
+//  Copyright © 2019 joy. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+
+
+- (void)log{
+    NSLog(@"I am a human");
+}
+@end
---
+++ JoySDK/Products/Standard/Aggregate.framework/Headers/Aggregate.h
@@ -0,0 +1,14 @@
+//
+//  Aggregate.h
+//  Aggregate
+//
+//  Created by joy on 2019/3/27.
+//  Copyright © 2019 joy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Aggregate/Aggregate.h>
+#import "Person.h"
+
+
+
