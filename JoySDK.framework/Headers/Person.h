be83c59ffaa2e399552e83760401da8396ca490f
joy <joy@joydeMacBook-Pro.local>
2019-05-09 10:32:09 +0800

    /

---
+++ JoySDK.framework/Headers/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  Aggregate
+//
+//  Created by joy on 2019/3/27.
+//  Copyright © 2019 joy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Person : NSObject
+
+
+- (void)log;
+@end
+
